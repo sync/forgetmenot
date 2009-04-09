5f488bd32f8313c22a88251b3bd7e1e4a32d535b
Anthony Mittaz <sync@me.com>
2009-04-09 16:00:16 +1000

    changed navigation bar tint color to grey

--- Classes/BaseViewController.m
+++ Classes/BaseViewController.m
@@ -38,7 +38,6 @@
 - (void)viewDidLoad {
 	[super viewDidLoad];
 	
-	//self.navigationController.navigationBar.barStyle = UIBarStyleBlack;
 	self.navigationController.navigationBar.tintColor = [UIColor colorWithRed:120.0/255.0 green:120.0/255.0 blue:120.0/255.0 alpha:1.0];
 	
 	[self loadAppDelegate];
