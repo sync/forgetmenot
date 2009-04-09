34addd08752ef5f18462536f1940658fd643221b
Anthony Mittaz <sync@me.com>
2009-04-09 15:19:07 +1000

    added Define header where you can define all shared macro etc...

---
+++ Define.h
@@ -0,0 +1,14 @@
+/*
+ *  Define.h
+ *  Latitude
+ *
+ *  Created by Anthony Mittaz on 1/04/09.
+ *  Copyright 2009 Anthony Mittaz. All rights reserved.
+ *
+ */
+
+#define ROW_HEIGHT 70.0
+
+// Networking
+#define kNetworkReachabilityChangedNotification @"kNetworkReachabilityChangedNotification"
+#define ShouldCancelAllOperationsDataFetcher @"ShouldCancelAllOperationsDataFetcher"
